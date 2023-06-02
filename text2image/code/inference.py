d7cc6ab303b549671a63e0439c072bb313965c17
Alexander Pivovarov <apivovarov@gmail.com>
2023-06-02 00:58:35 +0000

    Update text2image/code/inference.py

--- text2image/code/inference.py
+++ text2image/code/inference.py
@@ -1,8 +1,9 @@
 from diffusers import StableDiffusionPipeline
 import torch
 import base64
+import json
+import logging
 import numpy as np
-
 
 def process_data(data: dict) -> dict:
     return {
@@ -13,20 +14,25 @@ def process_data(data: dict) -> dict:
         "width": 512,
     }
 
-
 def model_fn(model_dir: str):
-    t2i_pipe = StableDiffusionPipeline.from_pretrained(
+    logging.info(f"Loading model from {model_dir=}")
+    t2i_pipe:StableDiffusionPipeline = StableDiffusionPipeline.from_pretrained(
         model_dir,
     )
     if torch.cuda.is_available():
         t2i_pipe = t2i_pipe.to("cuda")
 
     t2i_pipe.enable_attention_slicing()
+    logging.info(f"Model was loaded from {model_dir=}")
     return t2i_pipe
 
+def input_fn(input_data, content_type):
+    logging.debug(f"Run input_fn. {input_data=}, {content_type=}")
+    assert content_type == 'application/json', "Unexpected {content_type=}. Need application/json"
+    data = json.loads(input_data)
+    return data
 
 def predict_fn(data: dict, hgf_pipe) -> dict:
-
     with torch.autocast("cuda"):
         images = hgf_pipe(**process_data(data))["images"]
 
